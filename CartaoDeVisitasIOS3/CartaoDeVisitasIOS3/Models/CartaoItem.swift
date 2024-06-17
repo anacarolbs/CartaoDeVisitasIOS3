3ac9e70a4c1bb4c70d0ffa47ee3461384f343c52
Ana Carolina Barbosa de Souza <souzaanacarolb@gmail.com>
2024-06-17 01:30:27 -0300

    feat: create and save model

--- CartaoDeVisitasIOS3/CartaoDeVisitasIOS3/Models/CartaoItem.swift
+++ CartaoDeVisitasIOS3/CartaoDeVisitasIOS3/Models/CartaoItem.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct CartaoItem: Codable, Identifiable {
+    let id: String
+    let titleName: String
+    let titleEmail: String
+    let titlePhone: String
+    let titleJob: String
+    let titleLinkedin: String
+    let titleGithub: String
+    let createdDate: TimeInterval
+//    let isDone: Bool
+    
+//    mutating func setDone(_ state: Bool) {
+//        isDone = state
+//    }
+}
--- CartaoDeVisitasIOS3/CartaoDeVisitasIOS3/ViewModels/NewItemViewViewModel.swift
+++ CartaoDeVisitasIOS3/CartaoDeVisitasIOS3/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Ana Carolina Barbosa de Souza on 16/06/24.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -20,7 +22,34 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
+        // get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
         
+        //create model
+        let newId = UUID().uuidString
+        let newItem = CartaoItem(id: newId,
+                                 titleName: titleName,
+                                 titleEmail: titleEmail,
+                                 titlePhone: titlePhone,
+                                 titleJob: titleJob,
+                                 titleLinkedin: titleLinkedin,
+                                 titleGithub: titleGithub,
+                                 createdDate: Date().timeIntervalSince1970
+//                                 isDone: false
+        )
+        
+        //save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(newId)
+            .collection("cartões")
+            .document("123")
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
